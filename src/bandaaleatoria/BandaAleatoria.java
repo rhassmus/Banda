637bdc1b6fa8d36a0308e2b5bdf8c410447e602a
rhassmus <felipeagr_@hotmail.com>
2019-04-24 11:35:11 -0500

    clase

---
+++ src/bandaaleatoria/BandaAleatoria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandaaleatoria;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class BandaAleatoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
